f8be1e0462adaf3c1356e70ef059c9a6c6e3423b
AshfordLee <1207833942@qq.com>
2025-01-12 14:30:24 +0800

    2

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -10,21 +10,36 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(1) {
+        cache[0]=0;
+        cache[1]=1;
+    }
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &) = delete;
+    DynFibonacci(DynFibonacci const &other):cached(other.cached)
+    {
+        cache=new size_t[cached+1];
+        for (int i=0;i<=cached;i++)
+        {
+            cache[i]=other.cache[i];
+        }
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci()
+    {
+        delete[] cache;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        // 从当前 cached+1 算到 i
+        for (; cached < i; ++cached) {
+            cache[cached + 1] = cache[cached] + cache[cached - 1];
         }
         return cache[i];
     }
+
 
     // NOTICE: 不要修改这个方法
     // NOTICE: 名字相同参数也相同，但 const 修饰不同的方法是一对重载方法，可以同时存在
